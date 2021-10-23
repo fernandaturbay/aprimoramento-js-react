a6ba14f25c20011ed7752298266516366caa40ce
Fernanda Turbay <71790746+fernandaturbay@users.noreply.github.com>
2021-10-23 15:47:36 -0300

    aula if else

---
+++ JS/aula3/if-else.js
@@ -0,0 +1,12 @@
+//if permite criar condições
+
+const idade = 33
+
+if (idade <= 20) {
+    console.log ("você é adolescente")
+} else if (idade >= 21 && idade <= 30) {
+    console.log ("você é jovem")
+} else {
+    console.log ("você é adulto")
+}
+
